71fc6af153e27948ac1a43e9f5f49504f7595f87
Andrew Tangeman <andytangeman@gmail.com>
2021-07-14 00:28:48 -0700

    worked on sidebar formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
+  { path: '', component: AboutComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'projects', component: ProjectsComponent },
